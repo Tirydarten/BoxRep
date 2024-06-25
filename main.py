50956055e8d90649a7cb5b867d6c6e5f919a7784
Artem Volkov <tirydarten@mail.ru>
2024-06-25 17:21:18 +0700

    fixed version1.0

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from src.widget import mask_account_card, get_new_data
+from src.widget import get_new_data, mask_account_card
 
+print(mask_account_card("Visa Gold 5999414228426353"))
 
-print(mask_account_card("7000792289606361"))
-
-print(mask_account_card("73654108430135874305"))
+print(mask_account_card("Счет 73654108430135874305"))
 
 print(get_new_data("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -3,25 +3,17 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(number: str) -> str:
     """Функция получает строку и маскирует счет/карту"""
-    if len(number.split( )[-1]) == 16:
-        new_number = get_mask_card_number(number.split( )[-1])
+    if len(number.split()[-1]) == 16:
+        new_number = get_mask_card_number(number.split()[-1])
         result = f"{number[:-16]}{new_number}"
-        return result
-    elif len(number.split( )[-1]) == 20:
-        new_number = get_mask_account(number.split( )[-1])
+    elif len(number.split()[-1]) == 20:
+        new_number = get_mask_account(number.split()[-1])
         result = f"{number[:-20]}{new_number}"
-        return result
+    return result
 
-# print(mask_account_card("Maestro 1596837868705199"))
-# print(mask_account_card("MasterCard 7158300734726758"))
-# print(mask_account_card("Visa Platinum 8990922113665229"))
-# print(mask_account_card("Счет 73654108430135874305"))
 
-def get_new_data(old_data: str) ->str:
+def get_new_data(old_data: str) -> str:
     """Функция принимает строку с датой
-     и выводит в формате dd.mm.yyyy"""
+    и выводит в формате dd.mm.yyyy"""
     data_slize = old_data[0:10].split("-")
     return ".".join(data_slize[::-1])
-
-
-
